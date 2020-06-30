48572c62bba98b9600bbf51fc3b059dedeaa2a8d
Luis Moromisato <moromisato.luis@gmail.com>
2020-06-30 19:00:54 -0300

    created initial implementation of Home screen created Home screen styles.css

---
+++ frontend-challenge/src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './styles.css'
+
+export default function Home() {
+    return (
+        <div className="home-container">
+            <div className="header">
+                this is the header
+            </div>
+        </div>
+    )
+}
