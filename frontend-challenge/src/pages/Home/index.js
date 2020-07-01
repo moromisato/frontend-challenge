f574daf8f3e1bc8ea1b4ba13b05896cc6fccfa08
Luis Moromisato <moromisato.luis@gmail.com>
2020-06-30 23:37:48 -0300

    added useEffect to load mock data into home screen added IdentificationCard to Home added styles for home

--- frontend-challenge/src/pages/Home/index.js
+++ frontend-challenge/src/pages/Home/index.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Menu from '@material-ui/icons/Menu'
+import IdentifcationCard from '../../components/IdentifcationCard'
 
+import { User } from '../../mock'
 import './styles.css'
 
 export default function Home() {
+
+    const [ user, setUser ] = useState({})
+
+    useEffect(() => {
+        
+        console.log('chamado')
+        function loadUser() {
+            return User
+        }
+
+        setUser(loadUser)
+
+    }, [])
+
     return (
         <div className="home-container">
             <div className="header">
-                this is the header
+                <Menu />
+            </div>
+            <div className="body">
+                <IdentifcationCard user={user} />
             </div>
         </div>
     )
