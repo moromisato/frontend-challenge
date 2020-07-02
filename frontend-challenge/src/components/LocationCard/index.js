f53a353c3bb0d4cecb15b038afd4bf1edc1d6e8a
Luis Moromisato <moromisato.luis@gmail.com>
2020-07-02 00:40:14 -0300

    created LocationCard and styles.css

---
+++ frontend-challenge/src/components/LocationCard/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles.css'
+
+export default function LocationCard() {
+
+    return (
+        <div className="location-container">
+            <div className="location-title">
+                Vendas
+            </div>
+            <div className="location-map">
+                Adicionar um mapa aqui
+            </div>
+        </div>
+    )
+
+}
