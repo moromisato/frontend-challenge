9dc3370c4c53136dbdeaeae7155af5e35e36ab43
Luis Moromisato <moromisato.luis@gmail.com>
2020-07-01 22:01:54 -0300

    created SalesCard component preview created SalesCard styles.css

---
+++ frontend-challenge/src/components/SalesCard/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles.css'
+
+export default function SalesCard() {
+
+    return (
+        <div className="sales-container">
+            <div className="sales-title">
+                Vendas
+            </div>
+            <div className="sales-chart">
+                Adicionar um gráfico aqui
+            </div>
+        </div>
+    )
+
+}
