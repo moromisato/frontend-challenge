68bac9e1086549433300f59e84fe136eb808373d
Luis Moromisato <moromisato.luis@gmail.com>
2020-07-02 01:03:19 -0300

    fixed location-title

--- frontend-challenge/src/components/LocationCard/index.js
+++ frontend-challenge/src/components/LocationCard/index.js
@@ -6,7 +6,7 @@ export default function LocationCard() {
     return (
         <div className="location-container">
             <div className="location-title">
-                Vendas
+                Local
             </div>
             <div className="location-map">
                 Adicionar um mapa aqui
