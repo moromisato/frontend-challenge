341491eba97b3bd58cd80c7c128d0930bbd4ff4f
Luis Moromisato <moromisato.luis@gmail.com>
2020-07-03 18:26:02 -0300

    removed unused code

--- frontend-challenge/src/components/LocationCard/index.js
+++ frontend-challenge/src/components/LocationCard/index.js
@@ -11,17 +11,6 @@ export default function LocationCard() {
         return `https://${s}.tile.openstreetmap.org/${z}/${x}/${y}.png`
     }
 
-/*     osm: (x, y, z) => {
-        const s = String.fromCharCode(97 + (x + y + z) % 3)
-        return https://${s}.tile.openstreetmap.org/${z}/${x}/${y}.png
-      },
-      stamenTerrain: (x, y, z, dpr) => {
-        return https://stamen-tiles.a.ssl.fastly.net/terrain/${z}/${x}/${y}${dpr >= 2 ? '@2x' : ''}.jpg
-      },
-      stamenToner: (x, y, z, dpr) => {
-        return https://stamen-tiles.a.ssl.fastly.net/toner/${z}/${x}/${y}${dpr >= 2 ? '@2x' : ''}.png
-      } */
-
     return (
         <div className="location-container">
             <div className="location-title">
