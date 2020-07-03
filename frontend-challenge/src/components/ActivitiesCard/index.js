c94657f2960eabb0a29e6cdde38e20b7fa5681de
Luis Moromisato <moromisato.luis@gmail.com>
2020-07-02 21:06:54 -0300

    created ActivitiesCard component

---
+++ frontend-challenge/src/components/ActivitiesCard/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import './styles.css'
+
+export default function ActivitiesCard(props) {
+
+    return (
+        <div className="activities-container">
+
+            <div className="activities-title">
+                Atividades
+            </div>
+
+            <div className="activities-search">
+
+            </div>
+
+            <div className="activities-indicators">
+
+            </div>
+
+            <div className="activities-time-line">
+
+            </div>
+
+        </div>
+    )
+}
